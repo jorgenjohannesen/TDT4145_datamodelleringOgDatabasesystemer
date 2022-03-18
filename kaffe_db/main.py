4fb4bb74a3c20c2c83494005b497b72c51247898
jorgen <jorjo@ntnu.no>
2022-03-18 10:59:33 +0100

    Added user story 1 and validation for this

--- kaffe_db/main.py
+++ kaffe_db/main.py
@@ -1,13 +1,63 @@
-
 import sqlite3
+from sqlite3 import DatabaseError
 
 
-con = sqlite3.connect("coffee.db")
+def brukerhistorie1():
+    #Henter brenneri og sjekker at det finnes i databasen
+    while True:
+        try:
+            brenneri = erValidBrenneri(input("Hvilket brenneri kommer kaffen fra? "))
+        except DatabaseError:
+            print("dette brenneriet finnes ikke!")
+            continue
+        else:
+            break
 
-cursor = con.cursor()
-cursor.execute("INSERT into bruker VALUES (1, 'jrgen@test.no', 'Jørgen Johannesen' , 'testpassord')")
-cursor.execute("INSERT into bruker VALUES (2, 'jrgen@test.no', 'Jørgen Johannesen' , 'testpassord')")
+    #Henter kaffe fra bruker og sjekker om den finnes hos brenneriet
+    while True:
+        try:
+            kaffe = erValidKaffeFraBrenneri(input("Hva heter kaffen? "), brenneri)
+        except DatabaseError:
+            print("Brenneriet har ikke en kaffe som heter dette!")
+            continue
+        else:
+            break
 
-cursor.execute("SELECT brukerID FROM bruker")
-row = cursor.fetchall()
-print(row)
+    while True:
+        try:
+            poeng = int(input("Hvor mange poeng vil du gi denne kaffen? (1-10) "))
+        except ValueError:
+            print("Poeng må være et heltall ")
+            continue
+
+        if poeng > 10 or poeng < 1:
+            print("Vennligst oppgi et tall mellom 1 og 10")
+        else:
+            break
+
+def erValidBrenneri(brenneri):
+    connection = sqlite3.connect("coffee.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT navn FROM kaffebrenneri WHERE navn = ?", brenneri)
+    row = cursor.fetchone()
+    if len(row) <= 0:
+        raise DatabaseError
+    connection.close()
+
+def erValidKaffeFraBrenneri(kaffe, brenneri):
+    connection = sqlite3.connect("coffee.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * "
+                   "FROM kaffe JOIN kaffebrenneri USING brenneriID"
+                   " WHERE kaffe.navn = ? AND kaffebrenneri.navn = ?", (kaffe, brenneri))
+    row = cursor.fetchone()
+    if len(row) <= 0:
+        raise DatabaseError
+    connection.close()
+
+if __name__ == '__main__':
+    connection = sqlite3.connect("coffee.db")
+    cursor = connection.cursor()
+
+
+
