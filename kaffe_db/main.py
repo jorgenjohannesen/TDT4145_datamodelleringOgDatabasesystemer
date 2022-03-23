518fe285ac4036e1209c0959c7f20b6860cd4aec
jorgen <jorjo@ntnu.no>
2022-03-23 08:52:01 +0100

    Checkpoint

--- kaffe_db/main.py
+++ kaffe_db/main.py
@@ -3,23 +3,18 @@ from datetime import date
 from sqlite3 import DatabaseError
 
 
-
 def erValidEpostadresse(epostadresse):
     connection = sqlite3.connect("kaffe.db")
     cursor = connection.cursor()
-    cursor.execute(f"SELECT * FROM bruker WHERE epostadresse = {epostadresse}")
-    print("hei")
+    cursor.execute("SELECT * FROM bruker WHERE epostadresse = ?", (epostadresse,))
     row = cursor.fetchone()
-    print(row)
-    print(len(row))
     if len(row) <= 0:
         raise DatabaseError
     connection.close()
-    return row[0], row[2]
+    return row[0], row[3]
 
 
 def loggInn():
-
     while True:
         try:
             brukerID, passord = erValidEpostadresse(input("Skriv inn epostadresse: "))
@@ -38,34 +33,29 @@ def loggInn():
     return brukerID
 
 
-
-
-
-
-
-
-
 def erValidBrenneri(brenneri):
     connection = sqlite3.connect("kaffe.db")
     cursor = connection.cursor()
-    cursor.execute("SELECT navn FROM kaffebrenneri WHERE navn = ?", brenneri)
-    row = cursor.fetchone()
-
-    if len(row) <= 0:
-        raise DatabaseError
-    connection.close()
-
-def erValidKaffeFraBrenneri(kaffe, brenneri):
-    connection = sqlite3.connect("kaffe.db")
-    cursor = connection.cursor()
-    cursor.execute("SELECT kaffeID "
-                   "FROM kaffe JOIN kaffebrenneri USING brenneriID"
-                   " WHERE kaffe.navn = ? AND kaffebrenneri.navn = ?", (kaffe, brenneri))
+    cursor.execute("SELECT brenneriID FROM kaffebrenneri WHERE navn = ?", (brenneri,))
     row = cursor.fetchone()
     if len(row) <= 0:
         raise DatabaseError
     connection.close()
     return row[0]
+
+
+def erValidKaffeFraBrenneri(kaffe, brenneriID):
+    connection = sqlite3.connect("kaffe.db")
+    cursor = connection.cursor()
+    cursor.execute('''SELECT kaffeID 
+    FROM kaffe  
+    WHERE navn = ? AND brenneriID = ?''', (kaffe, brenneriID))
+    row = cursor.fetchone()
+    if len(row) <= 0:
+        raise DatabaseError
+    connection.close()
+    return row[0]
+
 
 def brukerhistorie1():
     brukerID = loggInn()
@@ -74,7 +64,7 @@ def brukerhistorie1():
 
     while True:
         try:
-            brenneri = erValidBrenneri(input("Hvilket brenneri kommer kaffen fra? "))
+            brenneriID = erValidBrenneri(input("Hvilket brenneri kommer kaffen fra? "))
         except DatabaseError:
             print("dette brenneriet finnes ikke!")
             continue
@@ -84,7 +74,7 @@ def brukerhistorie1():
         # Henter kaffe fra bruker og sjekker om den finnes hos brenneriet
     while True:
         try:
-            kaffeID = erValidKaffeFraBrenneri(input("Hva heter kaffen? "), brenneri)
+            kaffeID = erValidKaffeFraBrenneri(input("Hva heter kaffen? "), brenneriID)
         except DatabaseError:
             print("Brenneriet har ikke en kaffe som heter dette!")
             continue
@@ -105,10 +95,13 @@ def brukerhistorie1():
 
     notater = input("Skriv inn smaksnotatet: ")
     dato = input("Skriv inn dato (dd.mm.åååå) : ")
-    cursor.execute(f"Insert into kaffesmaking VALUES ({notater}, {poeng}, {dato}, {brukerID}, {kaffeID})")
-
+    connection = sqlite3.connect("kaffe.db")
+    cursor = connection.cursor()
+    cursor.execute('''Insert into kaffesmaking 
+    VALUES (?,?,?,?,?) ''', (notater, poeng, dato, brukerID, kaffeID))
+    connection.close()
 
 
 if __name__ == '__main__':
-    brukerhistorie1()
-
+    #brukerhistorie1()
+    
